4860b8e09c6f151687363a88a822f94a0e1d68f5
S0509 <S0509@21S0509>
2023-11-30 09:59:33 +0900

    B

---
+++ MyGitProject2d/src/main/java/com/example/demo/controller/MergeHellController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import java.util.Random;
+
+public class MergeHellController {
+	public static void main(String[] args) {
+
+		kadaiB();
+
+	}
+
+		static void kadaiB() {
+	//ここに課題を書く。メソッド名変えてね。
+			Random random = new Random();
+			int i = random.nextInt(100);
+			System.out.println(i + 1 + "が出ました。");
+
+		}
+
+}
